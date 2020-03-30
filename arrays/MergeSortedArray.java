7fa8cc522535b3f8275f4f925eabd0aa3afb0fde
st974 <st974@drexel.edu>
2020-03-30 19:34:47 -0400

    Added MergeSortedArray.java

---
+++ arrays/MergeSortedArray.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/merge-sorted-array/
+
+public class MergeSortedArray
+{
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        /* i points to middle of nums1, j points to end of nums2, 
+           k points to end of num1 */
+        int i = m - 1; 
+        int j = n - 1; 
+        int k = nums1.length - 1;
+        while (i >= 0 || j >= 0)
+        {
+            // i is exhausted
+            if (i < 0)
+                nums1[k--] = nums2[j--];
+            // j is exhausted, but i is already in nums1 and sorted
+            else if (j < 0)
+                break;
+            else if (nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else if (nums1[i] <= nums2[j])
+                nums1[k--] = nums2[j--];
+        }
+    }
+}
