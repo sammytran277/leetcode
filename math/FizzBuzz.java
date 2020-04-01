62171eb54bf79d89677a42dd980c15cd545419a0
st974 <st974@drexel.edu>
2020-03-31 20:51:48 -0400

    Added FizzBuzz.java

---
+++ math/FizzBuzz.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/fizz-buzz/
+
+import java.util.ArrayList;
+
+public class FizzBuzz
+{
+    public List<String> fizzBuzz(int n) 
+    {
+        ArrayList<String> ans = new ArrayList<String>();
+        
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                ans.add("FizzBuzz");
+            else if (i % 3 == 0)
+                ans.add("Fizz");
+            else if (i % 5 == 0)
+                ans.add("Buzz");
+            else
+                ans.add(String.valueOf(i));
+        }
+        
+        return ans;
+    }
+}
