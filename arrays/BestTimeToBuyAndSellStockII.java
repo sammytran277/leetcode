13ffa7f8953b87c4b7839b1de92d559dcd979ec7
st974 <st974@drexel.edu>
2020-04-05 10:31:49 -0400

    Added BestTimeToBuyAndSellStockII.java

---
+++ arrays/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+public class BestTimeToBuyAndSellStockII 
+{
+    // Another implementation of Kadane's algorithm
+    public int maxProfit(int[] prices) 
+    {
+        if (prices.length == 0)
+            return 0;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++)
+            profit += Math.max(prices[i] - prices[i - 1], 0);
+        return profit;
+    }
+}
