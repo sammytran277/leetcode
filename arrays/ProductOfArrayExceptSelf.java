e7761ebc2ba04e283bf350e86063daa15fc16919
st974 <st974@drexel.edu>
2020-04-15 09:49:01 -0400

    Added ProductOfArrayExceptSelf.java

--- arrays/ProductOfArrayExceptSelf.java
+++ arrays/ProductOfArrayExceptSelf.java
@@ -1,25 +1,32 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 
-import java.util.Arrays;
-
 public class ProductOfArrayExceptSelf 
 {
     public int[] productExceptSelf(int[] nums) 
     {
-        int[] products = new int[nums.length];
-        Arrays.fill(products, 1);
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+
+        // Prefix product
+        for (int i = 0; i < nums.length; i++)
+            prefix[i] = (i == 0) ? 1 : prefix[i - 1] * nums[i - 1];
+        
+        // Suffix product
+        for (int i = nums.length - 1; i >= 0; i--)
+            suffix[i] = (i == nums.length - 1) ? 1 : suffix[i + 1] * nums[i + 1];
+        
+        // Product except self is product of prefix and suffix
+        int[] product = new int[nums.length];
         for (int i = 0; i < nums.length; i++)
         {
-            if (nums[i] == 1)
-                continue;
-            for (int j = 0; j < nums.length; j++)
-            {
-                if (i == j)
-                    continue;
-                products[j] *= nums[i];
-            }
+            if (i == 0)
+                product[i] = suffix[i];
+            else if (i == nums.length - 1)
+                product[i] = prefix[i];
+            else
+                product[i] = prefix[i] * suffix[i];
         }
 
-        return products;
+        return product;
     }
 }
