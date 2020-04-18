4d2461e8be29fe0a7b988308e1edb1d6aba284f1
st974 <st974@drexel.edu>
2020-04-18 17:43:31 -0400

    Added a comment to HouseRobber.java

--- dynamic-programming/HouseRobber.java
+++ dynamic-programming/HouseRobber.java
@@ -7,6 +7,9 @@ public class HouseRobber
         int twoHousesAgo = 0, prevHouse = 0, currHouse;
         for (int i = 0; i < nums.length; i++)
         {
+            /* Idea is similar to MinCostClimbingStairs.java, the max you could've robbed
+               when you reach the i'th house is the current house and the max profit 
+               from two houses ago, or just the max profit from the previous house */
             currHouse = Math.max(nums[i] + twoHousesAgo, prevHouse);
             twoHousesAgo = prevHouse;
             prevHouse = currHouse;
