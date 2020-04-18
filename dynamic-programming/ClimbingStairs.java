2f776d3cb2eaa88a32a410e5ea9cd512b71e3f27
st974 <st974@drexel.edu>
2020-04-18 16:07:53 -0400

    Rewrote ClimbingStairs.java so it uses constant space

--- dynamic-programming/ClimbingStairs.java
+++ dynamic-programming/ClimbingStairs.java
@@ -1,20 +1,17 @@
 // https://leetcode.com/problems/climbing-stairs/
 
-import java.util.ArrayList;
-
 public class ClimbingStairs 
 {
     public int climbStairs(int n) 
     {
-        ArrayList<Integer> stairs = new ArrayList<Integer>();
-        
-        stairs.add(0);
-        stairs.add(1);
-        stairs.add(2);
-
-        for (int i = 3; i < n + 1; i++)
-            stairs.add(stairs.get(i - 1) + stairs.get(i - 2));
-        
-        return stairs.get(n);
+        int twoStepsAgo = 1, prevStep = 1, currStep = 0;
+        for (int i = 2; i < n + 1; i++)
+        {
+            currStep = twoStepsAgo + prevStep;
+            twoStepsAgo = prevStep;
+            prevStep = currStep;
+        }
+            
+        return prevStep;
     }
 }
