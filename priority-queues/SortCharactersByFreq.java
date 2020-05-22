0704b3f41e322234413a8a5efb912d2ece5898dd
st974 <st974@drexel.edu>
2020-05-22 09:06:09 -0400

    Added SortCharactersByFreq.java

---
+++ priority-queues/SortCharactersByFreq.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/sort-characters-by-frequency/
+
+public class SortCharactersByFreq 
+{
+    public String frequencySort(String s) 
+    {
+        HashMap<Character, Integer> charFreq = new HashMap<Character, Integer>();
+        PriorityQueue<Character> pq = new PriorityQueue(new Comparator<Character>() {
+            public int compare(Character a, Character b) 
+            {
+                if (charFreq.get(a) > charFreq.get(b))
+                    return -1;
+                else if (charFreq.get(a) < charFreq.get(b)) 
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+        
+        // Map each char to its letter frequency
+        for (char c : s.toCharArray())
+            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
+        
+        // Add each char to a max priority queue with a custom Comparator
+        for (char c : charFreq.keySet())
+            pq.add(c);
+        
+        // Create output string
+        StringBuilder sb = new StringBuilder();
+        while (pq.peek() != null) 
+        {
+            char c = pq.poll();
+            for (int i = 0; i < charFreq.get(c); i++)
+                sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
