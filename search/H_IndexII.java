de090eb1c581a82789efe78831900898d95d76b7
st974 <st974@drexel.edu>
2020-06-18 22:20:23 -0400

    Added H_IndexII.java

---
+++ search/H_IndexII.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/h-index-ii/
+
+public class H_IndexII {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (citations[mid] == n - mid)
+                return n - mid;
+            else if (citations[mid] < n - mid)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        
+        return n - low;
+    }
+}
