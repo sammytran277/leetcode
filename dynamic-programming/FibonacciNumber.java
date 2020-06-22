f81c93baeb242cb8b0de85b9bf4e7fb0f190ada5
st974 <st974@drexel.edu>
2020-06-21 20:48:21 -0400

    Added FibonacciNumber.java

---
+++ dynamic-programming/FibonacciNumber.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+public class FibonacciNumber {
+    // Bottom-up dynamic programming
+    public int fib(int N) {
+        if (N <= 1)
+            return N;
+        
+        int a = 0, b = 1, c;
+        for (int i = 2; i < N; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        
+        return a + b;
+    }
+}
