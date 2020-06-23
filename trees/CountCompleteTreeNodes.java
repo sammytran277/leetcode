bae144e4d08a365371cc7b3cb2dcd2e64b879d78
st974 <st974@drexel.edu>
2020-06-23 08:21:18 -0400

    Added CountCompleteTreeNodes.java

---
+++ trees/CountCompleteTreeNodes.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/count-complete-tree-nodes/
+
+public class CountCompleteTreeNodes {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
