47fb822ec5029b796dcbf4813883b4100035b016
st974 <st974@drexel.edu>
2020-06-25 09:17:32 -0400

    Added FindTheDuplicateNumber.java

---
+++ arrays/FindTheDuplicateNumber.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+import java.util.Arrays;
+
+public class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
