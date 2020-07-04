279076d9bb1b92361f02e6b42364e2677e9a73df
Sammy Tran <sammytran277@gmail.com>
2020-07-04 09:59:21 -0400

    Added UglyNumber.java

---
+++ math/UglyNumber.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/ugly-number/
+
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num <= 0)
+            return false;
+        
+        // Just keep dividing until you can't anymore
+        while (num % 2 == 0)
+            num /= 2;
+        while (num % 3 == 0)
+            num /= 3;
+        while (num % 5 == 0)
+            num /= 5;
+        
+        return num == 1;
+    }
+}
