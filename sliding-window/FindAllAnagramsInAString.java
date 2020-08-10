2269e7a72877be33d42700ead6279fe130dcc77c
Sammy Tran <sammytran277@gmail.com>
2020-08-10 11:26:45 -0400

    Rewrote solution to FindAllAnagramsInAString.java

--- sliding-window/FindAllAnagramsInAString.java
+++ sliding-window/FindAllAnagramsInAString.java
@@ -1,64 +1,64 @@
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
+import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FindAllAnagramsInAString 
 {
-    public List<Integer> findAnagrams(String s, String p) 
-    {
-        ArrayList<Integer> indexes = new ArrayList<Integer>();
-        HashMap<Character, Integer> charFreq = new HashMap<Character, Integer>();
-        int slow = 0, fast = -1;
-        
-        // Trivial case
-        if (s.length() < p.length()) 
-            return indexes;
-        
-        // Get char frequency of p
-        for (char c : p.toCharArray())
-            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
-        
-        int missing = charFreq.size();
-        while (fast < s.length() - 1) 
-        {
-            // Widen the window if the fast pointer is pointing at a char in p
-            fast++;
-            if (charFreq.containsKey(s.charAt(fast))) 
-            {
-                charFreq.put(s.charAt(fast), charFreq.get(s.charAt(fast)) - 1);
-                if (charFreq.get(s.charAt(fast)) == 0) 
-                    missing--;
-            } 
-            else 
-            {
-                // Shrink the window while a char that is not in p is in the window
-                while (slow <= fast) 
-                {
-                    if (charFreq.containsKey(s.charAt(slow))) 
-                    {
-                        if (charFreq.get(s.charAt(slow)) == 0) 
-                            missing++;
-                        charFreq.put(s.charAt(slow), charFreq.get(s.charAt(slow)) + 1);
-                    }
-                    
-                    slow++;
+    class Solution {
+        public List<Integer> findAnagrams(String s, String p) {
+            Map<Character, Integer> count = new HashMap<>();
+            List<Integer> indices = new ArrayList<>();
+            int lenS = s.length();
+            int lenP = p.length();
+            int lettersMatched = 0;
+            
+            // Trivial case
+            if (lenS < lenP)
+                return indices;
+            
+            // Add all chars of p as "costs"
+            for (char c : p.toCharArray()) {
+                count.put(c , count.getOrDefault(c, 0) + 1);
+            }
+            
+            // Grow window until size lenP
+            for (int i = 0; i < lenP; i++) {
+                if (count.containsKey(s.charAt(i))) {
+                    count.put(s.charAt(i), count.get(s.charAt(i)) - 1);
+                    if (count.get(s.charAt(i)) == 0)
+                        lettersMatched++;
                 }
             }
-
-            // Once the window is of size p, check if it is an anagram of p
-            if (fast - slow + 1 == p.length()) 
-            {
-                if (missing == 0) 
-                    indexes.add(slow);
-                if (charFreq.get(s.charAt(slow)) == 0) 
-                    missing++;
-                charFreq.put(s.charAt(slow), charFreq.get(s.charAt(slow)) + 1);
-                slow++;
+            
+            // Check for first window being an anagram of p
+            if (lettersMatched == count.size())
+                indices.add(0);
+            
+            // Sliding window with fixed size
+            for (int end = lenP; end < lenS; end++) {
+                // Increase the window from the right by one
+                if (count.containsKey(s.charAt(end))) {
+                    count.put(s.charAt(end), count.get(s.charAt(end)) - 1);
+                    if (count.get(s.charAt(end)) == 0)
+                        lettersMatched++;
+                }
+                
+                // Decrease the window from the left by one
+                if (count.containsKey(s.charAt(end - lenP))) {
+                    count.put(s.charAt(end - lenP), count.get(s.charAt(end - lenP)) + 1);
+                    if (count.get(s.charAt(end - lenP)) == 1)
+                        lettersMatched--;
+                }
+                
+                // Check for matches
+                if (lettersMatched == count.size())
+                    indices.add(end - lenP + 1);
             }
+            
+            return indices;
         }
-        
-        return indexes;
     }
 }
