dff793f8cf0030a1b43422bfb054b35045c9f6e1
Sammy Tran <sammytran277@gmail.com>
2020-09-18 11:22:14 -0400

    Rewrote the solution to FindAllNumbersDisappearedInAnArray.java

--- arrays/FindAllNumbersDisappearedInAnArray.java
+++ arrays/FindAllNumbersDisappearedInAnArray.java
@@ -1,39 +1,31 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
-public class FindAllNumbersDisappearedInAnArray 
-{
-    public List<Integer> findDisappearedNumbers(int[] nums) 
-    {
-        ArrayList<Integer> missing = new ArrayList<Integer>();
-        Arrays.sort(nums);
-        
-        for (int i = 1; i <= nums.length; i++) 
-        {
-            if (!binarySearch(nums, i))
-                missing.add(i);
+public class FindAllNumbersDisappearedInAnArray  {
+    public List<Integer> findDisappearedNumbers(int[] A) {
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] != i + 1 && A[i] != A[A[i] - 1]) {
+                swap(A, i, A[i] - 1);
+            } else {
+                i++;
+            }
         }
         
-        return missing;
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] != j + 1) {
+                ans.add(j + 1);
+            }
+        }
+        
+        return ans;
     }
     
-    public boolean binarySearch(int[] A, int n) 
-    {
-        int left = 0, right = A.length - 1, mid;
-        while (left <= right) 
-        {
-            mid = left + (right - left) / 2;
-            if (A[mid] == n)
-                return true;
-            else if (A[mid] > n)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
-        
-        return false;
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
