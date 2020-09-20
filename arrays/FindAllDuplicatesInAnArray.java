9570594a05f52a32cdbd482707458298f839b60a
Sammy Tran <sammytran277@gmail.com>
2020-09-19 22:31:59 -0400

    Rewrote the solution to FindAllDuplicatesInAnArray.java

--- arrays/FindAllDuplicatesInAnArray.java
+++ arrays/FindAllDuplicatesInAnArray.java
@@ -1,20 +1,31 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
 
-import java.util.List;
-import java.util.HashSet;
-import java.util.ArrayList;
+import java.util.*;
 
-public class FindAllDuplicatesInAnArray {
-    public List<Integer> findDuplicates(int[] nums) {
-        HashSet<Integer> unique = new HashSet<>();
-        List<Integer> duplicates = new ArrayList<>();
-        for (int num: nums) {
-            if (!unique.contains(num))
-                unique.add(num);
-            else
-                duplicates.add(num);
+class Solution {
+    public List<Integer> findDuplicates(int[] A) {
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] != A[A[i] - 1]) {
+                swap(A, i, A[i] - 1);
+            } else {
+                i++;
+            }
         }
         
-        return duplicates;
+        List<Integer> ans = new ArrayList<>(); 
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] != j + 1) {
+                ans.add(A[j]);
+            }
+        }
+        
+        return ans;
+    }
+    
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
