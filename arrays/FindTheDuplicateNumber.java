3422758774edf4f9d0cd35b970d880cfe456391a
Sammy Tran <sammytran277@gmail.com>
2020-09-19 22:25:19 -0400

    Rewrote the solution to FindTheDuplicateNumber.java

--- arrays/FindTheDuplicateNumber.java
+++ arrays/FindTheDuplicateNumber.java
@@ -1,16 +1,28 @@
 // https://leetcode.com/problems/find-the-duplicate-number/
 
-import java.util.Arrays;
-
 public class FindTheDuplicateNumber {
-    public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
-                return nums[i];
+    public int findDuplicate(int[] A) {
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] != A[A[i] - 1]) {
+                swap(A, i, A[i] - 1);
+            } else {
+                i++;
             }
         }
-
+        
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] != j + 1) {
+                return A[j];
+            }
+        }
+        
         return -1;
     }
+    
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    } 
 }
