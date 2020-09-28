a5104ca9fcff23f51aca9b1b536bbe4c5aea1922
Sammy Tran <sammytran277@gmail.com>
2020-09-28 09:21:52 -0400

    Rewrote solution to BinaryTreeLevelOrderTraversal.java

--- trees/BinaryTreeLevelOrderTraversal.java
+++ trees/BinaryTreeLevelOrderTraversal.java
@@ -1,34 +1,37 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal/
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class BinaryTreeLevelOrderTraversal {
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) {
+            return levels;
+        }
+        
         LinkedList<TreeNode> q = new LinkedList<>();
-
-        if (root != null)
-            q.addFirst(root);
+        q.add(root);
         
         while (q.size() != 0) {
-            int n = q.size();
-            ArrayList<Integer> level = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            int N = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
                 TreeNode currNode = q.pollFirst();
-                if (currNode.left != null)
-                    q.add(currNode.left);
-                if (currNode.right != null)
-                    q.add(currNode.right);
                 level.add(currNode.val);
+                
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
             }
             
             levels.add(level);
