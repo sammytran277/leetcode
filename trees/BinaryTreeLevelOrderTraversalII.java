cae7c9066419b0767bcba469a88018d4ade2cdad
Sammy Tran <sammytran277@gmail.com>
2020-09-28 09:35:39 -0400

    Rewrote the solution to BinaryTreeLevelOrderTraversalII.java

--- trees/BinaryTreeLevelOrderTraversalII.java
+++ trees/BinaryTreeLevelOrderTraversalII.java
@@ -1,40 +1,42 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class BinaryTreeLevelOrderTraversalII {
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
     }
 
-    // Same as Level Order Traversal I, but insert the level at index 0
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) {
+            return levels;
+        }
+        
         LinkedList<TreeNode> q = new LinkedList<>();
-
-        if (root != null)
-            q.addFirst(root);
+        q.add(root);
         
         while (q.size() != 0) {
-            int n = q.size();
-            ArrayList<Integer> level = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            int N = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
                 TreeNode currNode = q.pollFirst();
-                if (currNode.left != null)
-                    q.add(currNode.left);
-                if (currNode.right != null)
-                    q.add(currNode.right);
                 level.add(currNode.val);
+                
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
             }
             
             levels.add(0, level);
         }
-        
+
         return levels;
     }
 }
