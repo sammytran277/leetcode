553716dc9efbec77c00ae8c5296de15e85441a33
Sammy Tran <sammytran277@gmail.com>
2020-10-11 17:33:10 -0400

    Rewrote solution to BinaryTreeMaxPathSum.java

--- trees/BinaryTreeMaxPathSum.java
+++ trees/BinaryTreeMaxPathSum.java
@@ -1,41 +1,28 @@
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
 
-public class BinaryTreeMaxPathSum 
-{
-    public class TreeNode 
-    {
+public class BinaryTreeMaxPathSum  {
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) 
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 
-    int max = Integer.MIN_VALUE;
-    public int maxPathSum(TreeNode root) 
-    {
-        helper(root);
-        return max;
+    int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        findMax(root);
+        return maxSum;
     }
     
-    int helper(TreeNode root) 
-    {
-        if (root == null) 
+    public int findMax(TreeNode root) {
+        if (root == null) {
             return 0;
+        }
         
-        // Returns the best path from each child, or 0 if it is better to not take that path
-        int left = Math.max(helper(root.left), 0);
-        int right = Math.max(helper(root.right), 0);
+        int maxLeft = Math.max(findMax(root.left), 0);
+        int maxRight = Math.max(findMax(root.right), 0);
+        maxSum = Math.max(maxSum, root.val + maxLeft + maxRight);
         
-        // Updates the global max if taking all three nodes leads to a better max
-        max = Math.max(max, root.val + left + right);
+        return root.val + Math.max(maxLeft, maxRight);
         
-        return root.val + Math.max(left, right);
     }
 }
