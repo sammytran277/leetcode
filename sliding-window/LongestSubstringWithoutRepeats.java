b4a018c4136a5d1f3865eb17b90a888a70a69f98
Sammy Tran <sammytran277@gmail.com>
2020-10-21 15:52:29 -0400

    Rewrote solution to LongestSubstringWithoutRepeats.java

--- sliding-window/LongestSubstringWithoutRepeats.java
+++ sliding-window/LongestSubstringWithoutRepeats.java
@@ -1,26 +1,24 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-import java.util.HashSet;
+import java.util.*;
 
-public class LongestSubstringWithoutRepeats 
-{
-    public int lengthOfLongestSubstring(String s) 
-    {
-        int slow = 0, fast = 0, max = 0;
-        HashSet<Character> windowChars = new HashSet<Character>();
-        while (fast < s.length())
-        {
-            if (windowChars.contains(s.charAt(fast)))
-            {
-                windowChars.remove(s.charAt(slow));
-                slow++;
-                continue;
+public class LongestSubstringWithoutRepeats {
+    public int lengthOfLongestSubstring(String s) {
+        int N = s.length();
+        int start = 0;
+        int longest = 0;
+        Map<Character, Integer> lastIndex = new HashMap<>();
+
+        for (int end = 0; end < N; end++) {
+            char c = s.charAt(end);
+            if (lastIndex.containsKey(c) && lastIndex.get(c) >= start) {
+                start = lastIndex.get(c) + 1;
             }
-            max = Math.max(max, fast - slow + 1);
-            windowChars.add(s.charAt(fast));
-            fast++;
+            
+            lastIndex.put(s.charAt(end), end);
+            longest = Math.max(longest, end - start + 1);
         }
         
-        return max;
+        return longest;
     }
 }
