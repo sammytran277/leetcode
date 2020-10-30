ce59312385e377762bc19bb272a9cf4a944d25e7
Sammy Tran <sammytran277@gmail.com>
2020-10-30 09:59:15 -0400

    Rewrote the solution to MinStack.java

--- design/MinStack.java
+++ design/MinStack.java
@@ -1,65 +1,45 @@
 // https://leetcode.com/problems/min-stack/
 
-import java.util.LinkedList;
-
-public class MinStack 
-{
-    LinkedList<Integer> stack;
-    int indexOfMin;
-
-    // Constructor
-    public MinStack() 
-    {
-        stack = new LinkedList<Integer>();
-        indexOfMin = -1;
+public class MinStack {  
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+        
+        public Node(int val, int min) {
+            this(val, min, null);
+        }
+    }    
+    
+    private Node head;
+    
+    public MinStack() {
+        head = null;
     }
     
-    // Push an int onto the stack
-    public void push(int x) 
-    {
-        if (indexOfMin == -1)
-            indexOfMin = 0;
-        else if (x < (int) stack.get(indexOfMin))
-            indexOfMin = stack.size();
-        stack.add(x);
-    }
-    
-    // Pop the last int from the stack
-    public void pop() 
-    {
-        stack.removeLast();
-        // Find the new min if we popped it off the stack
-        if (indexOfMin >= stack.size())
-        {
-            // If the stack is empty, there is no min
-            if (stack.size() == 0)
-            {
-                indexOfMin = -1;
-                return;
-            }
-            
-            // Iterate through the stack to find the index of the new min
-            int newMin = Integer.MAX_VALUE;
-            for (int i = 0; i < stack.size(); i++)
-            {
-                if ((int) stack.get(i) <= newMin)
-                {
-                    indexOfMin = i;
-                    newMin = (int) stack.get(i);
-                }
-            }
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x);
+        } else {
+            head = new Node(x, Math.min(x, head.min), head);
         }
     }
     
-    // Peek at the last int on the stack
-    public int top() 
-    {
-        return (int) stack.peekLast();
+    public void pop() {
+        head = head.next;
     }
     
-    // Return the int with the lowest value from the stack
-    public int getMin() 
-    {
-        return (int) stack.get(indexOfMin);
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
     }
 }
