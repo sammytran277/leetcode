dbaf6f755e88605987b5840f8a9c1b3b03bc72f8
Sammy Tran <sammytran277@gmail.com>
2021-02-11 22:40:55 -0500

    Added CheckIfNAndItsDoubleExists.java

---
+++ arrays/CheckIfNAndItsDoubleExists.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+public class CheckIfNAndItsDoubleExists {
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
