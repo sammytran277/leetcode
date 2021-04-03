4ff0e1aeb8404e2cdf7342ab21e54b57a516496b
Sammy Tran <sammytran277@gmail.com>
2021-04-03 17:02:31 -0400

    Added problems to binary search folder

--- search/SearchInRotatedSortedArray.java
+++ search/SearchInRotatedSortedArray.java
@@ -1,31 +1,28 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-public class SearchRotatedSortedArray
-{
-    public int search(int[] nums, int target) 
-    {
-        int left = 0, right = nums.length - 1, mid;
-        while (left <= right)
-        {
-            mid = left + (right - left) / 2;
-            if (nums[mid] == target)
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            else if (nums[left] == target)
-                return left;
-            else if (nums[right] == target)
-                return right;
-            else if (target > nums[left] && target < nums[mid])
-                right = mid - 1;
-            else if (target > nums[mid] && target < nums[right])
-                left = mid + 1;
-            else if (target < nums[left] && target < nums[right] && nums[left] > nums[mid])
-                right = mid - 1;
-            else if (target > nums[left] && target > nums[mid] && nums[left] > nums[mid])
-                right = mid - 1;
-            else if (target < nums[left] && target < nums[mid] && nums[left] > nums[mid])
-                right = mid - 1;
-            else
-                left = mid + 1;
+            } else if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         
         return -1;
