a7f5cbe54d633313b1ab6377ddb3f52af0509e62
Sammy Tran <sammytran277@gmail.com>
2021-04-17 12:21:03 -0400

    Added MinimumOperationsToMakeTheArrayIncreasing.java

---
+++ arrays/MinimumOperationsToMaketheArrayIncreasing.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
+
+public class MinimumOperationsToMaketheArrayIncreasing {
+    public int minOperations(int[] nums) {
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                res += (nums[i - 1] - nums[i]) + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        
+        return res;    
+}
