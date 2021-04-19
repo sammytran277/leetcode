e22aecc0f259c4c8b8bee57a4aa836bc4cb83a96
Sammy Tran <sammytran277@gmail.com>
2021-04-19 09:54:26 -0400

    Resolved LongestCommonSubsequence.java

--- dynamic-programming/LongestCommonSubsequence.java
+++ dynamic-programming/LongestCommonSubsequence.java
@@ -1,26 +1,20 @@
 // https://leetcode.com/problems/longest-common-subsequence/
 
-public class LongestCommonSubsequence 
-{
-    public int longestCommonSubsequence(String text1, String text2) 
-    {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        
-        for (int i = 0; i < m + 1; i++)
-        {
-            for (int j = 0; j < n + 1; j++)
-            {
-                if (i == 0 || j == 0)
-                    continue;
-                else if (text1.charAt(i - 1) == text2.charAt(j - 1))
+public class LongestCommonSubsequence { 
+    public int longestCommonSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
+                } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
         
-        return dp[m][n];
+        return dp[n][m];
     }
 }
