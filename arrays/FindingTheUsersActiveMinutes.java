d097f9091e6c4ea8f5639c53fe8ee77c4c41199e
Sammy Tran <sammytran277@gmail.com>
2021-04-29 19:45:58 -0400

    Added FindingTheUsersActiveMinutes.java

---
+++ arrays/FindingTheUsersActiveMinutes.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/finding-the-users-active-minutes/
+
+public class FindingTheUsersActiveMinutes {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] log : logs) {
+            map.putIfAbsent(log[0], new HashSet<>());
+            map.get(log[0]).add(log[1]);
+        }
+        
+        int[] res = new int[k];
+        for (Set<Integer> set : map.values()) {
+            res[set.size() - 1]++;
+        }
+        
+        return res;
+    }    
+}
