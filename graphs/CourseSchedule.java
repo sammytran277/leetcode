cc327cebb9666f22c0135a7481b1c036decf4a94
Sammy Tran <sammytran277@gmail.com>
2021-05-04 10:26:17 -0400

    Rewrote the solution to CourseSchedule.java

--- graphs/CourseSchedule.java
+++ graphs/CourseSchedule.java
@@ -1,58 +1,39 @@
 // https://leetcode.com/problems/course-schedule/
 
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
-public class CourseSchedule 
-{
-    // Standard topological sort implementation, look for existence of cycles
-    public boolean canFinish(int numCourses, int[][] prerequisites) 
-    {
-        HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<Integer, ArrayList<Integer>>();
-        HashMap<Integer, Integer> inDegree = new HashMap<Integer, Integer>();
-        LinkedList<Integer> toVisit = new LinkedList<Integer>();
-        
-        // Initialize adjacency list and in-degree mapping
-        for (int i = 0; i < numCourses; i++) 
-        {
-            adjList.put(i, new ArrayList<Integer>());
-            inDegree.put(i, 0);
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<>();
         }
         
-        // Represent graph as adjacency list and map each course to number of prerequisites
-        for (int[] prereq : prerequisites) 
-        {
-            adjList.get(prereq[1]).add(prereq[0]);
-            inDegree.put(prereq[0], inDegree.get(prereq[0]) + 1);
-        }
-
-        // Add all courses with in-degree = 0 to queue
-        for (int course : adjList.keySet()) 
-        {
-            if (inDegree.get(course) == 0)
-                toVisit.add(course);
+        int[] inDegree = new int[numCourses];
+        for (int[] prereq : prerequisites) {
+            adjList[prereq[1]].add(prereq[0]);
+            inDegree[prereq[0]]++;
         }
         
-        // If there are no courses with in-degree = 0, there must be a cycle
-        if (toVisit.size() == 0)
-            return false;
-        
-        int coursesTaken = 0;
-        while (toVisit.size() != 0) 
-        {
-            int currCourse = toVisit.removeFirst();
-            coursesTaken++;
-            
-            // Decrement the in-degree of all neighboring courses
-            for (int neighbor : adjList.get(currCourse)) 
-            {
-                inDegree.put(neighbor, inDegree.get(neighbor) - 1);
-                if (inDegree.get(neighbor) == 0)
-                    toVisit.add(neighbor);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
             }
         }
         
-        return (coursesTaken == numCourses) ? true : false;
+        int numVisited = 0;
+        while (!q.isEmpty()) {
+            int currCourse = q.poll();
+            numVisited++;
+            for (int dependent : adjList[currCourse]) {
+                inDegree[dependent]--;
+                if (inDegree[dependent] == 0) {
+                    q.add(dependent);
+                }
+            }
+        }
+        
+        return numVisited == numCourses;
     }
 }
