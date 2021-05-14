f24103e2256a4fd625b6a523c672a3dfaa6fbf23
Sammy Tran <sammytran277@gmail.com>
2021-05-14 10:01:52 -0400

    Added SetMatrixZeroes.java

---
+++ arrays/SetMatrixZeroes.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/set-matrix-zeroes/
+
+import java.util.*;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> zeroCols = new HashSet<>();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (zeroRows.contains(i) || zeroCols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }    
+}
